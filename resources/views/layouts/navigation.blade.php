0df960ef9a7df3a0779f0da0d2669236b42f8bb2
AarinWilt <118760362+AarinWilt@users.noreply.github.com>
2022-11-21 12:18:55 -0500

    Add files via upload

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,21 +25,19 @@
                         {{ __('Calculator') }}
                     </x-nav-link>
 
-                        <x-nav-link href="/visit" :active="request()->routeIs('visit')">
+                        <x-nav-link href="/visit" :active="request()->routeIs('/visit/index')">
                             {{ __('View Visit Schedule') }}
                         </x-nav-link>
+                        @if (Auth::user()->role == 'const.admin')
+                        <x-nav-link href="/userlist" :active="request()->routeIs('/userlist/index')">
+                            {{ __('User list') }}
+                        </x-nav-link>
+                        @endif
                     @if (Auth::user()->role == 'const.userDefault')
                     <x-nav-link href="/visit/create" :active="request()->routeIs('/visit/create')">
                             {{ __('Schedule a Visit') }}
                         </x-nav-link>
                     @endif
-
-                    @if (Auth::user()->role == 'const.admin')
-                    <x-nav-link href="/queries" :active="request()->routeIs('/queries')">
-                            {{ __('Queries') }}
-                        </x-nav-link>
-                    @endif
-                   
                 </div>
             </div>
 
