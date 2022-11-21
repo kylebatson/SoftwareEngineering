194d754aed7691e69ff9b9fe9ef1d2684a8cb863
AarinWilt <118760362+AarinWilt@users.noreply.github.com>
2022-11-21 12:39:34 -0500

    Small changes iteration 4 team 3

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,33 +23,31 @@
                     </x-nav-link>
                     <x-nav-link :href="url('calculator')" :active="request()->routeIs('calculator')">
                         {{ __('Calculator') }}
+                    </x-nav-link>
+
+                        <x-nav-link href="/visit" :active="request()->routeIs('/visit/index')">
+                            {{ __('View Visit Schedule') }}
+                        </x-nav-link>
+
+                    @if (Auth::user()->role == 'const.userDefault')
+                    <x-nav-link href="/visit/create" :active="request()->routeIs('/visit/create')">
+                            {{ __('Schedule a Visit') }}
+                        </x-nav-link>
+                    @endif
+
+                    @if (Auth::user()->role == 'const.admin')
+                        <x-nav-link href="/userlist" :active="request()->routeIs('/userlist/index')">
+                            {{ __('User list') }}
+                        </x-nav-link>
+                    @endif
+                    @if (Auth::user()->role == 'const.admin')
+                    <x-nav-link href="/queries" :active="request()->routeIs('/queries')">
+                            {{ __('Queries') }}
+                        </x-nav-link>
+                    @endif
                     
-</x-nav-link>
-
-<x-nav-link href="/visit" :active="request()->routeIs('visit')">
-<x-nav-link href="/visit" :active="request()->routeIs('/visit/index')">
-    {{ __('View Visit Schedule') }}
-</x-nav-link>
-@if (Auth::user()->role == 'const.admin')
-<x-nav-link href="/userlist" :active="request()->routeIs('/userlist/index')">
-    {{ __('User list') }}
-</x-nav-link>
-@endif
-@if (Auth::user()->role == 'const.userDefault')
-<x-nav-link href="/visit/create" :active="request()->routeIs('/visit/create')">
-    {{ __('Schedule a Visit') }}
-</x-nav-link>
-@endif
-
-@if (Auth::user()->role == 'const.admin')
-<x-nav-link href="/queries" :active="request()->routeIs('/queries')">
-    {{ __('Queries') }}
-</x-nav-link>
-@endif
-
-</div>
-</div>
-
+                </div>
+            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
